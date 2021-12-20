34c669c01726e94fdc4c7b074ab7eb1ca1bc0b57
Kai <kai_lochi@gmx.de>
2021-12-20 12:11:58 +0100

    plot distributions of Td and distance

--- src/plot_overview.R
+++ src/plot_overview.R
@@ -5,6 +5,9 @@ library(RColorBrewer)
 # load saved tracks file
 load(file = paste(dir_data, "tracks.database.RData", sep = ""))
 
+# convert distance to km
+tracks.database$Td_distance <- tracks.database$Td_distance / 1000
+
 # colors for scaling diagram
 colors <- brewer.pal(5, "Set1")
 
@@ -21,7 +24,6 @@ for (season in 1:3) {
 #
 # plot descriptive stats about dew point temperature
 #
-tracks.database$Td_distance <- tracks.database$Td_distance / 1000
 pl <-
   ggplot(tracks.database,
          aes(x = Td_distance, group = season, color = season)) +
@@ -45,7 +47,7 @@ pl <-
   )
 #pl
 ggsave(
-  paste(dir_plots, "Td_distance_by_season.png", sep = ""),
+  paste(dir_plots, "Td_distance_dist_by_season.png", sep = ""),
   plot = pl,
   width = 6,
   height = 8,
@@ -55,7 +57,7 @@ ggsave(
 pl <-
   ggplot(tracks.database, aes(x = Td_value, group = season, color = season)) +
   geom_freqpoly(binwidth = 0.3) +
-  xlab(expression(paste(T[d]," [",degree,"C]"))) + xlim(-10, 25) +
+  xlab(expression(paste(T[d], " [", degree, "C]"))) + xlim(-10, 25) +
   theme_bw(base_size = pl.basesize) +
   theme(
     panel.spacing = unit(0, "line"),
@@ -73,34 +75,102 @@ pl <-
     axis.title.y = element_text(size = pl.basesize)
   )
 ggsave(
-  paste(dir_plots, "Td_value_by_season.png", sep = ""),
+  paste(dir_plots, "Td_value_dist_by_season.png", sep = ""),
   plot = pl,
   width = 6,
   height = 8,
   units = "cm"
 )
 
+# cumulative distributions by season
+prct <- seq(0, 1, 0.0001)
+
 cumdist.distance <-
-  quantile(
-    tracks.database$Td_distance / 1000,
-    probs = seq(0, 1, 0.001),
+  aggregate(
+    tracks.database$Td_distance,
+    by = list(tracks.database$season),
+    FUN = quantile,
+    probs = prct,
     type = 5,
-    na.rm = T
+    na.rm = TRUE
   )
-cumdist.distance <- melt(cumdist.distance)
-cumdist.distance$x <- seq(0, 1, 0.001) * 100
+cumdist.distance <- melt(cumdist.distance$x)
+colnames(cumdist.distance) <- c("season","percentile","distance")
+cumdist.distance$percentile <- rep(prct,each=3)*100
+cumdist.distance$season <- seasons[cumdist.distance$season]
+
+# plot
+pl <- ggplot(cumdist.distance, aes(x = distance, y = percentile, group=season,color=season)) +
+  geom_path() +
+  xlab("distance [km]") +
+  ylab("percentile [%]") +
+  xlim(0, 100) +
+  theme_bw(base_size = pl.basesize) +
+  theme(
+    panel.spacing = unit(0, "line"),
+    strip.background = element_blank(),
+    strip.placement = 'outside',
+    strip.text = element_text(size = pl.basesize),
+    legend.position = "None",
+    legend.justification = c("right", "top"),
+    legend.background = element_blank(),
+    plot.margin = margin(0.1, 0.05, 0.1, 0.1, "cm"),
+    axis.text = element_text(size = pl.basesize),
+    legend.title = element_blank(),
+    legend.text = element_text(size = pl.basesize),
+    legend.key.height = unit(.5, "line"),
+    axis.title.y = element_text(size = pl.basesize)
+  )
+ggsave(
+  paste(dir_plots, "Td_distance_cumdist_by_season.png", sep = ""),
+  plot = pl,
+  width = 6,
+  height = 8,
+  units = "cm"
+)
+
+
 cumdist.Td <-
-  quantile(
+  aggregate(
     tracks.database$Td_value,
-    probs = seq(0, 1, 0.001),
+    by = list(tracks.database$season),
+    FUN = quantile,
+    probs = prct,
     type = 5,
-    na.rm = T
+    na.rm = TRUE
   )
-cumdist.Td <- melt(cumdist.Td)
-cumdist.Td$x <- seq(0, 1, 0.001) * 100
+cumdist.Td <- melt(cumdist.Td$x)
+colnames(cumdist.Td) <- c("season","percentile","Td")
+cumdist.Td$percentile <- rep(prct,each=3)*100
+cumdist.Td$season <- seasons[cumdist.Td$season]
 
-pl <- ggplot(cumdist.distance, aes(x = value, y = x)) + geom_line()
-pl
 
-pl <- ggplot(cumdist.Td, aes(x = value, y = x)) + geom_line()
-pl
+pl <- ggplot(cumdist.Td, aes(x = Td, y = percentile, color=season, group=season)) +
+  geom_line() +
+  xlab(expression(paste(T[d], " [", degree, "C]"))) +
+  ylab("percentile [%]") +
+  xlim(-10, 25) +
+  theme_bw(base_size = pl.basesize) +
+  theme(
+    panel.spacing = unit(0, "line"),
+    strip.background = element_blank(),
+    strip.placement = 'outside',
+    strip.text = element_text(size = pl.basesize),
+    legend.position = "None",
+    legend.justification = c("right", "top"),
+    legend.background = element_blank(),
+    plot.margin = margin(0.1, 0.05, 0.1, 0.1, "cm"),
+    axis.text = element_text(size = pl.basesize),
+    legend.title = element_blank(),
+    legend.text = element_text(size = pl.basesize),
+    legend.key.height = unit(.5, "line"),
+    axis.title.y = element_text(size = pl.basesize)
+  )
+ggsave(
+  paste(dir_plots, "Td_value_cumdist_by_season.png", sep = ""),
+  plot = pl,
+  width = 6,
+  height = 8,
+  units = "cm"
+)
+

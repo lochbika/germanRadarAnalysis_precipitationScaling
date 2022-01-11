de0c534e12391264bf11d91b666b63098e43f03b
Kai Lochbihler <kai.lochbihler@gmx.de>
2022-01-11 15:07:41 +0000

    connect tracks to precipitation type

--- src/connect_tracks_with_gridded_precipitationtype.R
+++ src/connect_tracks_with_gridded_precipitationtype.R
@@ -1,14 +1,14 @@
 library(geosphere)
 
 # load saved tracks file
-load(file = paste(dir_data, "tracks.global.RData", sep = ""))
+load(file = paste(dir_data, "tracks.database.RData", sep = ""))
 grid.definition <-
   read.csv(file = paste(dir_data, "grid_CS_type_aggregatoion.csv", sep = ""))
 
 # calculate the distance matrix
 trackgriddistance <- distm(as.matrix(cbind(
-  tracks.global$coorLon,
-  tracks.global$coorLat
+  tracks.database$coorLon,
+  tracks.database$coorLat
 )),
 as.matrix(cbind(grid.definition$X,
                 grid.definition$Y)))
@@ -45,7 +45,7 @@ save(
   tracksandgrid,
   file = paste(
     dir_data,
-    "tracksglobalandgrid_IDs_distance_cloud_type_Q1_Q2_gridded.RData",
+    "tracksandgrid_IDs_distance_cloud_type_Q1_Q2_gridded.RData",
     sep = ""
   )
 )
@@ -56,26 +56,25 @@ rm(gridstationids.ordered, trackgriddistance.ordered)
 load(
   paste(
     dir_data,
-    "tracksglobalandgrid_IDs_distance_cloud_type_Q1_Q2_gridded.RData",
+    "tracksandgrid_IDs_distance_cloud_type_Q1_Q2_gridded.RData",
     sep = ""
   )
 )
-load(file = paste(dir_data, "parallel_hourly_gridded_cloudtypes_Q1_Q2_timeseries.RData", sep = ""))
+load(file = paste(dir_data, "gridded_precipitationType_Q1_Q2_timeseries.RData", sep = ""))
 
 # create temporary vectors for cloud types
 CS_grid_valueQ1 <-
-  rep(-1, dim(tracks.global)[1])
+  rep(-1, dim(tracks.database)[1])
 CS_grid_valueQ2 <-
-  rep(-1, dim(tracks.global)[1])
+  rep(-1, dim(tracks.database)[1])
 CS_grid_distance <-
-  rep(-1, dim(tracks.global)[1])
+  rep(-1, dim(tracks.database)[1])
 # generate dates for gridded data from dimnames
 grid.cloudtypes.date <-
-  as.POSIXct(as.numeric(dimnames(grid.cloudtypes.timeseries)$date), tz = "UTC",
-             origin = "1970-01-01 00:00")
+  as.POSIXct(dimnames(grid.precipitationType.timeseries)$date, tz = "UTC")
 
 # loop tracks
-for (track in 1:dim(tracks.global)[1]) {
+for (track in 1:dim(tracks.database)[1]) {
   if (track %% 1000 == 0)
     print(track)
   # closest grid point
@@ -83,25 +82,25 @@ for (track in 1:dim(tracks.global)[1]) {
   # closest date for track in gridded data; get only index
   closestDate <-
     which.min(abs(
-      difftime(grid.cloudtypes.date, tracks.global$datetime[track])
+      difftime(grid.cloudtypes.date, tracks.database$datetime[track])
     ))
   
   # select the record
   CS_grid_distance[track] <-
     tracksandgrid$distances[track]
   CS_grid_valueQ1[track] <-
-    grid.cloudtypes.timeseries[closestGridoint, 1, closestDate]
+    grid.precipitationType.timeseries[closestGridoint, 1, closestDate]
   CS_grid_valueQ2[track] <-
-    grid.cloudtypes.timeseries[closestGridoint, 2, closestDate]
+    grid.precipitationType.timeseries[closestGridoint, 2, closestDate]
 }
 
 # add the temporary fields to tracks.global
-tracks.global$CS_grid_distance <- CS_grid_distance
-tracks.global$CS_grid_valueQ1 <- CS_grid_valueQ1
-tracks.global$CS_grid_valueQ2 <- CS_grid_valueQ2
+tracks.database$CS_grid_distance <- CS_grid_distance
+tracks.database$CS_grid_valueQ1 <- CS_grid_valueQ1
+tracks.database$CS_grid_valueQ2 <- CS_grid_valueQ2
 rm(CS_grid_distance, CS_grid_valueQ1, CS_grid_valueQ2)
 
 # update tracks file
-save(tracks.global,
-     file = paste(dir_data, "tracks.global.RData", sep =
+save(tracks.database,
+     file = paste(dir_data, "tracks.database.RData", sep =
                     ""))
